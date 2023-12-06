fd568dd86d345be93a3394b8e151e3762fc42668
khushbu <khushbusoni475@gmail.com>
2023-12-06 17:40:49 +0530

    [khushbu] modify employees attendence

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,20 @@
 package Day4_EmployeeWage_PP;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to the employee wage program");
+        final int IS_PRESENT = 1;
+        Random random = new Random();
+        int randomNumber = random.nextInt(2);
+        if(IS_PRESENT == randomNumber)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
     }
 }
