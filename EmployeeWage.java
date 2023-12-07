15293c42b66fadf85afbfc0c98f3aced15e1686a
khushbu <khushbusoni475@gmail.com>
2023-12-07 15:02:28 +0530

    [khushbu] modify part time employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,20 +7,25 @@ public class EmployeeWage {
         System.out.println("Welcome to the employee wage program");
         //final int IS_PRESENT = 1;
         final int FULL_TIME = 1;
+        final int PART_TIME = 2;
         final int WAGE_PER_HRS = 20;
         int empWage = 0;
         int empHrs = 0;
         Random random = new Random();
-        int randomNumber = random.nextInt(2);
+        int randomNumber = random.nextInt(3);
         if(FULL_TIME == randomNumber)
         {
-            System.out.println("Employee is present");
+            System.out.println("Full time employee is present");
             empHrs = 8;
-        }
-        else
+        } else if (PART_TIME == randomNumber)
+        {
+            System.out.println("Part time employee is present");
+            empHrs = 4;
+        } else
         {
             System.out.println("Employee is absent");
         }
+
         empWage = empHrs * WAGE_PER_HRS;
         System.out.println("Employee daily wage : " + empWage);
     }
