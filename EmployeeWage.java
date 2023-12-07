9e1317721f8de8e59a77e189dd7739748abbffaf
khushbu <khushbusoni475@gmail.com>
2023-12-07 15:32:35 +0530

    [khushbu] modify employee wage for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,26 +9,34 @@ public class EmployeeWage {
         final int FULL_TIME = 1;
         final int PART_TIME = 2;
         final int WAGE_PER_HRS = 20;
+        final int MAX_WORKING_DAYS = 20;
         int empWage = 0;
         int empHrs = 0;
+        int totalWage = 0;
         Random random = new Random();
-        int randomNumber = random.nextInt(3);
-
-        switch (randomNumber)
+        for (int i = 0; i<MAX_WORKING_DAYS; i++)
         {
-            case FULL_TIME:
-                System.out.println("Full time employee is present");
-                empHrs = 8;
-                break;
-            case PART_TIME:
-                System.out.println("Part time employee is present");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
-                break;
+            int randomNumber = random.nextInt(3);
+            switch (randomNumber)
+            {
+                case FULL_TIME:
+                    System.out.println("Full time employee is present");
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    System.out.println("Part time employee is present");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+                    break;
+            }
+            empWage = empHrs * WAGE_PER_HRS;
+            totalWage = totalWage + empWage;
+            System.out.println("Employee daily wage : " + empWage);
         }
-        empWage = empHrs * WAGE_PER_HRS;
-        System.out.println("Employee daily wage : " + empWage);
+
+
+        System.out.println("TotalWage for" + MAX_WORKING_DAYS + " days is: " + totalWage);
     }
 }
