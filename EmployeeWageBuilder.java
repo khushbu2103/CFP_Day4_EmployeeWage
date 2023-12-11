0a067089c3a7fd65f6e59e4ad663d47769daa732
khushbu <khushbusoni475@gmail.com>
2023-12-11 17:44:47 +0530

    [khushbu] adding save total wage for each company

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,19 +1,29 @@
 package Day4_EmployeeWage_PP;
 
 import java.util.Random;
-public class EmployeeWage {
-    final int FULL_TIME = 1;
-    final int PART_TIME = 2;
-    final int WAGE_PER_HRS = 20;
-    final int MAX_WORKING_DAYS = 20;
-    final int MAX_WORKING_HRS = 100;
-    int empWage = 0;
-    int empHrs = 0;
-    int totalWage = 0;
-    int day = 1;
-    int totalHrs = 0;
+public class EmployeeWageBuilder {
+    public String company;
+    public int maxWorkingDay, maxWorkingHrs, empRatePerHr;
+    public EmployeeWageBuilder(String company, int maxWorkingDay, int maxWorkingHrs, int empRatePerHr)
+    {
+        this.company = company;
+        this.maxWorkingDay = maxWorkingDay;
+        this.maxWorkingHrs = maxWorkingHrs;
+        this.empRatePerHr = empRatePerHr;
+    }
 
-    public void CalculateWage(String company, int maxWorkingDay, int maxWorkingHrs, int empRatePerHr) {
+
+    public void CalculateWage() {
+        final int FULL_TIME = 1;
+        final int PART_TIME = 2;
+        final int WAGE_PER_HRS = 20;
+        final int MAX_WORKING_DAYS = 20;
+        final int MAX_WORKING_HRS = 100;
+        int empWage = 0;
+        int empHrs = 0;
+        int totalWage = 0;
+        int day = 1;
+        int totalHrs = 0;
         System.out.println("Welcome to the employee wage program");
         //final int IS_PRESENT = 1;
 
@@ -45,10 +55,13 @@ public class EmployeeWage {
 
 class EmployeeWage2 {
     public static void main(String[] args) {
-        EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.CalculateWage("Delloit" , 24, 120, 40);
-        employeeWage.CalculateWage("Microsoft", 34, 100, 46);
-        employeeWage.CalculateWage("Bridgelabz", 20, 130, 35);
+        EmployeeWageBuilder deloitte = new EmployeeWageBuilder("Deloitte", 24, 120, 40);
+        deloitte.CalculateWage();
+        EmployeeWageBuilder Microsoft = new EmployeeWageBuilder("Microsoft", 34, 100, 46);
+        Microsoft.CalculateWage();
+        EmployeeWageBuilder Google = new EmployeeWageBuilder("Google", 20, 130, 35);
+        Google.CalculateWage();
+
     }
     }
 
